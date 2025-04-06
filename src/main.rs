291a974867a375dea574702fc3c97b8becd280f0
car <carl.hedgren@gmail.com>
2025-04-06 12:41:02 +0200

    A trivial api

---
+++ src/main.rs
@@ -0,0 +1,17 @@
+use axum::{routing::get, Router};
+use tracing::{self, Level};
+
+async fn say_hello() -> Result<String, (axum::http::StatusCode, String)> {
+    Ok("hello".to_string())
+}
+
+#[tokio::main]
+async fn main() {
+    tracing_subscriber::fmt().with_max_level(Level::INFO).init();
+    tracing::info!("starting trivial-api");
+    let app = Router::new().route("/hello", get(say_hello));
+    let listener = tokio::net::TcpListener::bind("0.0.0.0:3000")
+        .await
+        .expect("failed to bind port");
+    axum::serve(listener, app).await.unwrap();
+}
