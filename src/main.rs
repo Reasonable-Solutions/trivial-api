367e016c03bc2dec4f289b97c543cefef9aa098a
car <carl.hedgren@gmail.com>
2025-04-08 00:16:55 +0200

    fix routing

--- src/main.rs
+++ src/main.rs
@@ -9,7 +9,7 @@ async fn say_hello() -> Result<String, (axum::http::StatusCode, String)> {
 async fn main() {
     tracing_subscriber::fmt().with_max_level(Level::INFO).init();
     tracing::info!("starting trivial-api");
-    let app = Router::new().route("/hello", get(say_hello));
+    let app = Router::new().route("/trivial", get(say_hello));
     let listener = tokio::net::TcpListener::bind("0.0.0.0:3000")
         .await
         .expect("failed to bind port");
