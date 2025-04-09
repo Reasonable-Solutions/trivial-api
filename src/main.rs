6ec767ea94879f28f5db905b383d89e1360dd2a1
car <carl.hedgren@gmail.com>
2025-04-09 23:10:50 +0200

    Add a readme

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,15 @@
 use axum::{routing::get, Router};
-use tracing::{self, Level};
+use tracing::{self, info, Level};
 
 async fn say_hello() -> Result<String, (axum::http::StatusCode, String)> {
-    tracing::info!("[trivial-api] cool request");
+    info!("[trivial-api] hi");
     Ok("hello".to_string())
 }
 
 #[tokio::main]
 async fn main() {
-    let thisiSaClippyWarning = 5;
     tracing_subscriber::fmt().with_max_level(Level::INFO).init();
-    tracing::info!("starting trivial-api");
+    info!("starting trivial-api");
     let app = Router::new().route("/trivial", get(say_hello));
     let listener = tokio::net::TcpListener::bind("0.0.0.0:3000")
         .await
